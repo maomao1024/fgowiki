f44f09151f83021a41ffcdc144e8b74cb4ba8f12
jiangjing <jiangjing@iri.cn>
2017-12-15 16:43:54 +0800

    1

--- fgowiki-spider/src/main/java/com/github/fgowiki/spider/instance/html/FgoServantPageProcesser.java
+++ fgowiki-spider/src/main/java/com/github/fgowiki/spider/instance/html/FgoServantPageProcesser.java
@@ -2,6 +2,7 @@ package com.github.fgowiki.spider.instance.html;
 
 import com.alibaba.fastjson.JSONObject;
 import com.github.fgowiki.spider.model.FgoServant;
+import com.github.fgowiki.spider.utils.FgoCamp;
 import com.github.fgowiki.spider.utils.ObjectUtil;
 import com.github.fgowiki.spider.utils.HibernateUtil;
 import org.apache.commons.lang3.StringUtils;
@@ -13,7 +14,7 @@ import us.codecraft.webmagic.selector.Html;
 import us.codecraft.webmagic.selector.Selectable;
 
 import java.text.MessageFormat;
-import java.util.List;
+import java.util.*;
 
 /**
  * 英灵信息
@@ -23,64 +24,83 @@ import java.util.List;
  **/
 public class FgoServantPageProcesser implements PageProcessor {
 
-    private Site site = Site.me().setRetryTimes(0).setSleepTime(1000);
-    private static List<FgoServant> servantList;
-    private static int index = 1;
-    private static final String url = "http://fgowiki.com/guide/petdetail/{0}";
+	private Site site = Site.me().setRetryTimes(0).setSleepTime(1000);
+	private static List<FgoServant> servantList;
+	Map<String,String> map=new HashMap<>();
+	private static int index = 3;
+	private static final String url = "http://fgowiki.com/guide/petdetail/{0}";
 
-    @Override
-    public void process(Page page) {
-        System.out.println("=====================================================");
-        System.out.println(page.getUrl());
-        Html html = page.getHtml();
-        Selectable script = html.xpath("//*[@id=\"row-move\"]/div[2]/div/script[9]");
-        String datadetailString = script.regex("var datadetail = \\[(.+?)\\];").get();
-        if (StringUtils.isNotBlank(datadetailString)) {
-            JSONObject datadetail = JSONObject.parseObject(datadetailString);
-            saveData(datadetail);
-        }
+	@Override
+	public void process(Page page) {
+		System.out.println("=====================================================");
+		System.out.println(page.getUrl());
+		Html html = page.getHtml();
+		Selectable script = html.xpath("//*[@id=\"row-move\"]/div[2]/div/script[9]");
+		String datadetailString = script.regex("var datadetail = \\[(.+?)\\];").get();
+		if (StringUtils.isNotBlank(datadetailString)) {
+			JSONObject datadetail = JSONObject.parseObject(datadetailString);
+			saveData(datadetail);
+			index++;
+			page.addTargetRequest(getNextRequest());
+		}else {
+			map.forEach((key,value)-> System.out.println(key));
+		}
 
-        System.out.println("=====================================================");
-    }
+		System.out.println("=====================================================");
+	}
 
-    public void saveData(JSONObject data) {
-        FgoServant servant = (FgoServant) HibernateUtil.get(FgoServant.class, data.getInteger("ID"));
-        if (servant != null) {
-            //data.forEach((key, value) -> System.out.println(key + "\t" + value));
-            String camp = data.getString("Camp");
-            Integer star = data.getInteger("STAR");
-            Integer card_quick = data.getInteger("CARD_QUICK");
-            Integer card_arts = data.getInteger("CARD_ARTS");
-            Integer card_buster = data.getInteger("CARD_BUSTER");
-            String cards = ObjectUtil.getStringBylength("A", card_arts) + ObjectUtil.getStringBylength("B", card_buster) + ObjectUtil.getStringBylength("Q", card_quick);
-            Integer lv90_atk = data.getInteger("LV90_ATK");
-            Integer lv90_hp = data.getInteger("LV90_HP");
-            Integer lv100_atk = data.getInteger("LV100_ATK");
-            Integer lv100_hp = data.getInteger("LV100_HP");
-            Integer exHit = data.getInteger("EXHit");
-            Integer artHit = data.getInteger("ArtHit");
-            Integer busHit = data.getInteger("BusHit");
-            Integer quiHit = data.getInteger("QuiHit");
-            String painter = data.getString("ILLUST");
-            String cv = data.getString("CV");
+	public void saveData(JSONObject data) {
+		map.put(data.getString("Attributes"),"");
+		/*FgoServant servant = (FgoServant) HibernateUtil.get(FgoServant.class, data.getInteger("ID"));
+		if (servant != null) {
+			String campStr = data.getString("Camp");
+			Integer camp = FgoCamp.getCamp(campStr);
+			Integer star = data.getInteger("STAR");
+			Integer card_quick = data.getInteger("CARD_QUICK");
+			Integer card_arts = data.getInteger("CARD_ARTS");
+			Integer card_buster = data.getInteger("CARD_BUSTER");
+			String cards = ObjectUtil.getStringBylength("A", card_arts) + ObjectUtil.getStringBylength("B", card_buster) + ObjectUtil.getStringBylength("Q", card_quick);
+			Integer lv90_atk = data.getInteger("LV90_ATK");
+			Integer lv90_hp = data.getInteger("LV90_HP");
+			Integer lv100_atk = data.getInteger("LV100_ATK");
+			Integer lv100_hp = data.getInteger("LV100_HP");
+			Integer exHit = data.getInteger("EXHit");
+			Integer artHit = data.getInteger("ArtHit");
+			Integer busHit = data.getInteger("BusHit");
+			Integer quiHit = data.getInteger("QuiHit");
+			String painter = data.getString("ILLUST");
+			String cv = data.getString("CV");
+			servant.setCamp(camp);
+			servant.setStar(star);
+			servant.setCards(cards);
+			servant.setAtkLv90(lv90_atk);
+			servant.setHpLv90(lv90_hp);
+			servant.setAtkLv100(lv100_atk);
+			servant.setHpLv100(lv90_hp);
+			servant.setEtxtraHit(exHit);
+			servant.setBusterHit(busHit);
+			servant.setArtHit(artHit);
+			servant.setQuickHit(quiHit);
+			servant.setPainter(painter);
+			servant.setCv(cv);
 
-        }
-    }
+		}*/
+	}
 
-    @Override
-    public Site getSite() {
-        return site;
-    }
+	@Override
+	public Site getSite() {
+		return site;
+	}
 
-    public static void main(String[] args) {
+	public static void main(String[] args) {
         /*Object list = HibernateUtil.queryForlist("FROM FgoServant");
         servantList = (List<FgoServant>) list;*/
-        Spider spider = Spider.create(new FgoServantPageProcesser());
-        spider.addUrl(MessageFormat.format(url, 3));
-        spider.thread(1).run();
-    }
+		Spider spider = Spider.create(new FgoServantPageProcesser());
+		spider.addUrl(MessageFormat.format(url, 3));
+		spider.thread(1).run();
+	}
 
-    private static String getNextRequest() {
-        return MessageFormat.format(url, index);
-    }
+	private static String getNextRequest() {
+		return MessageFormat.format(url, index);
+	}
 }
