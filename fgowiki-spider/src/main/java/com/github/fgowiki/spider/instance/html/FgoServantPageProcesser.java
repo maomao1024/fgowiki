28a2c562af833b618f4969cf358a10838be88e6a
jiangjing <jiangjing@iri.cn>
2017-12-20 17:33:36 +0800

    完善初始数据

--- fgowiki-spider/src/main/java/com/github/fgowiki/spider/instance/html/FgoServantPageProcesser.java
+++ fgowiki-spider/src/main/java/com/github/fgowiki/spider/instance/html/FgoServantPageProcesser.java
@@ -26,8 +26,8 @@ public class FgoServantPageProcesser implements PageProcessor {
 
 	private Site site = Site.me().setRetryTimes(0).setSleepTime(1000);
 	private static List<FgoServant> servantList;
-	Map<String,String> map=new HashMap<>();
-	private static int index = 3;
+	Map<String, String> map = new HashMap<>();
+	private static int index = 114;
 	private static final String url = "http://fgowiki.com/guide/petdetail/{0}";
 
 	@Override
@@ -42,8 +42,8 @@ public class FgoServantPageProcesser implements PageProcessor {
 			saveData(datadetail);
 			index++;
 			page.addTargetRequest(getNextRequest());
-		}else {
-			map.forEach((key,value)-> System.out.println(key));
+		} else {
+			map.forEach((key, value) -> System.out.println(key));
 		}
 
 		System.out.println("=====================================================");
@@ -52,37 +52,57 @@ public class FgoServantPageProcesser implements PageProcessor {
 	public void saveData(JSONObject data) {
 		FgoServant servant = (FgoServant) HibernateUtil.get(FgoServant.class, data.getInteger("ID"));
 		if (servant != null) {
-			String campStr = data.getString("Camp");
-			Integer camp = FgoCamp.getCamp(campStr);
-			Integer star = data.getInteger("STAR");
-			Integer card_quick = data.getInteger("CARD_QUICK");
-			Integer card_arts = data.getInteger("CARD_ARTS");
-			Integer card_buster = data.getInteger("CARD_BUSTER");
-			String cards = ObjectUtil.getStringBylength("A", card_arts) + ObjectUtil.getStringBylength("B", card_buster) + ObjectUtil.getStringBylength("Q", card_quick);
-			Integer lv90_atk = data.getInteger("LV90_ATK");
-			Integer lv90_hp = data.getInteger("LV90_HP");
-			Integer lv100_atk = data.getInteger("LV100_ATK");
-			Integer lv100_hp = data.getInteger("LV100_HP");
-			Integer exHit = data.getInteger("EXHit");
-			Integer artHit = data.getInteger("ArtHit");
-			Integer busHit = data.getInteger("BusHit");
-			Integer quiHit = data.getInteger("QuiHit");
-			String painter = data.getString("ILLUST");
-			String cv = data.getString("CV");
-			servant.setCamp(camp);
-			servant.setStar(star);
-			servant.setCards(cards);
-			servant.setAtkLv90(lv90_atk);
-			servant.setHpLv90(lv90_hp);
-			servant.setAtkLv100(lv100_atk);
-			servant.setHpLv100(lv90_hp);
-			servant.setEtxtraHit(exHit);
-			servant.setBusterHit(busHit);
-			servant.setArtHit(artHit);
-			servant.setQuickHit(quiHit);
-			servant.setPainter(painter);
-			servant.setCv(cv);
+			try {
+				String campStr = data.getString("Camp");
+				Integer camp = FgoCamp.getCamp(campStr);
+				Integer star = data.getInteger("STAR");
+				Integer card_quick = data.getInteger("CARD_QUICK");
+				Integer card_arts = data.getInteger("CARD_ARTS");
+				Integer card_buster = data.getInteger("CARD_BUSTER");
+				String cards = ObjectUtil.getStringBylength("A", card_arts) + ObjectUtil.getStringBylength("B", card_buster) + ObjectUtil.getStringBylength("Q", card_quick);
+				Integer lv90_atk = data.getInteger("LV90_ATK");
+				Integer lv90_hp = data.getInteger("LV90_HP");
+				Integer lv100_atk = data.getInteger("LV100_ATK");
+				Integer lv100_hp = data.getInteger("LV100_HP");
+				Integer exHit = data.getInteger("EXHit");
+				Integer artHit = data.getInteger("ArtHit");
+				Integer busHit = data.getInteger("BusHit");
+				Integer quiHit = data.getInteger("QuiHit");
+				String painter = data.getString("ILLUST");
+				String cv = data.getString("CV");
+				String orgin = data.getString("Origin");
+				String region = data.getString("Region");
+				Integer Height = null;
+				Integer Weight = null;
+				try {
+					Height = Integer.parseInt(removeString(data.getString("Height")));
+				} catch (NumberFormatException e) {
+					System.out.println(servant.getNameZh() + "-Weight:" + data.getString("Height").toLowerCase());
+				}
+				try {
+					Weight = Integer.parseInt(removeString(data.getString("Weight")));
+				} catch (NumberFormatException e) {
+					System.out.println(servant.getNameZh() + "-Weight:" + data.getString("Weight").toLowerCase());
+				}
+				String gender = data.getString("Gender");
 
+				servant.setCamp(camp);
+				servant.setCards(cards);
+				servant.setAtkLv90(lv90_atk);
+				servant.setHpLv90(lv90_hp);
+				servant.setAtkLv100(lv100_atk);
+				servant.setHpLv100(lv100_hp);
+				servant.setPainter(painter);
+				servant.setCv(cv);
+				servant.setOrigin(orgin);
+				servant.setGender(gender == null ? 0 : (gender.contains("女") ? 0 : 1));
+				servant.setRegion(region);
+				servant.setHeight(Height);
+				servant.setWeight(Weight);
+				HibernateUtil.saveOrUpdate(servant);
+			} catch (Exception e) {
+				e.printStackTrace();
+			}
 		}
 	}
 
@@ -95,11 +115,20 @@ public class FgoServantPageProcesser implements PageProcessor {
         /*Object list = HibernateUtil.queryForlist("FROM FgoServant");
         servantList = (List<FgoServant>) list;*/
 		Spider spider = Spider.create(new FgoServantPageProcesser());
-		spider.addUrl(MessageFormat.format(url, 3));
+		spider.addUrl(MessageFormat.format(url, index));
 		spider.thread(1).run();
 	}
 
 	private static String getNextRequest() {
 		return MessageFormat.format(url, index);
+	}
+
+
+	private String removeString(String str) {
+		String[] rem = {"cm", "CM", "KG", "kg", "?", "？", "㎝"};
+		for (String s : rem) {
+			str = str.replace(s, "");
+		}
+		return str;
 	}
 }
--- fgowiki-spider/src/main/java/com/github/fgowiki/spider/model/FgoServant.java
+++ fgowiki-spider/src/main/java/com/github/fgowiki/spider/model/FgoServant.java
@@ -8,7 +8,7 @@ import java.util.Objects;
  *
  * @author 蒋经
  * @version 1.0
- * @since 2017/12/15
+ * @since 2017/12/20
  */
 @Entity
 @Table(name = "FGO_SERVANT")
@@ -18,8 +18,8 @@ public class FgoServant {
 	private String nameEn;
 	private String nameJp;
 	private Integer clazz;
-	private Integer camp;
 	private Integer star;
+	private Integer camp;
 	private Integer atkStage0;
 	private Integer hpStage0;
 	private String cards;
@@ -32,7 +32,7 @@ public class FgoServant {
 	private Integer artHit;
 	private Integer busterHit;
 	private Integer quickHit;
-	private Integer etxtraHit;
+	private Integer extraHit;
 	private String painter;
 	private String cv;
 	private String attributes;
@@ -40,14 +40,14 @@ public class FgoServant {
 	private String region;
 	private Integer height;
 	private Integer weight;
-	private Integer critStar;
+	private Double critStar;
 	private Integer crit;
-	private Integer npRateArt;
-	private Integer npRateBuster;
-	private Integer npRateQuick;
-	private Integer npRateExtra;
+	private Double npRateArt;
+	private Double npRateBuster;
+	private Double npRateQuick;
+	private Double npRateExtra;
 	private String origin;
-	private Integer npRateAttacked;
+	private Double npRateAttacked;
 	private String nickname;
 
 	@Id
@@ -101,16 +101,6 @@ public class FgoServant {
 	}
 
 	@Basic
-	@Column(name = "CAMP")
-	public Integer getCamp() {
-		return camp;
-	}
-
-	public void setCamp(Integer camp) {
-		this.camp = camp;
-	}
-
-	@Basic
 	@Column(name = "STAR")
 	public Integer getStar() {
 		return star;
@@ -118,6 +108,16 @@ public class FgoServant {
 
 	public void setStar(Integer star) {
 		this.star = star;
+	}
+
+	@Basic
+	@Column(name = "CAMP")
+	public Integer getCamp() {
+		return camp;
+	}
+
+	public void setCamp(Integer camp) {
+		this.camp = camp;
 	}
 
 	@Basic
@@ -241,13 +241,13 @@ public class FgoServant {
 	}
 
 	@Basic
-	@Column(name = "ETXTRA_HIT")
-	public Integer getEtxtraHit() {
-		return etxtraHit;
+	@Column(name = "EXTRA_HIT")
+	public Integer getExtraHit() {
+		return extraHit;
 	}
 
-	public void setEtxtraHit(Integer etxtraHit) {
-		this.etxtraHit = etxtraHit;
+	public void setExtraHit(Integer extraHit) {
+		this.extraHit = extraHit;
 	}
 
 	@Basic
@@ -322,11 +322,11 @@ public class FgoServant {
 
 	@Basic
 	@Column(name = "CRIT_STAR")
-	public Integer getCritStar() {
+	public Double getCritStar() {
 		return critStar;
 	}
 
-	public void setCritStar(Integer critStar) {
+	public void setCritStar(Double critStar) {
 		this.critStar = critStar;
 	}
 
@@ -342,41 +342,41 @@ public class FgoServant {
 
 	@Basic
 	@Column(name = "NP_RATE_ART")
-	public Integer getNpRateArt() {
+	public Double getNpRateArt() {
 		return npRateArt;
 	}
 
-	public void setNpRateArt(Integer npRateArt) {
+	public void setNpRateArt(Double npRateArt) {
 		this.npRateArt = npRateArt;
 	}
 
 	@Basic
 	@Column(name = "NP_RATE_BUSTER")
-	public Integer getNpRateBuster() {
+	public Double getNpRateBuster() {
 		return npRateBuster;
 	}
 
-	public void setNpRateBuster(Integer npRateBuster) {
+	public void setNpRateBuster(Double npRateBuster) {
 		this.npRateBuster = npRateBuster;
 	}
 
 	@Basic
 	@Column(name = "NP_RATE_QUICK")
-	public Integer getNpRateQuick() {
+	public Double getNpRateQuick() {
 		return npRateQuick;
 	}
 
-	public void setNpRateQuick(Integer npRateQuick) {
+	public void setNpRateQuick(Double npRateQuick) {
 		this.npRateQuick = npRateQuick;
 	}
 
 	@Basic
 	@Column(name = "NP_RATE_EXTRA")
-	public Integer getNpRateExtra() {
+	public Double getNpRateExtra() {
 		return npRateExtra;
 	}
 
-	public void setNpRateExtra(Integer npRateExtra) {
+	public void setNpRateExtra(Double npRateExtra) {
 		this.npRateExtra = npRateExtra;
 	}
 
@@ -392,11 +392,11 @@ public class FgoServant {
 
 	@Basic
 	@Column(name = "NP_RATE_ATTACKED")
-	public Integer getNpRateAttacked() {
+	public Double getNpRateAttacked() {
 		return npRateAttacked;
 	}
 
-	public void setNpRateAttacked(Integer npRateAttacked) {
+	public void setNpRateAttacked(Double npRateAttacked) {
 		this.npRateAttacked = npRateAttacked;
 	}
 
@@ -410,52 +410,4 @@ public class FgoServant {
 		this.nickname = nickname;
 	}
 
-	@Override
-	public boolean equals(Object o) {
-		if (this == o) return true;
-		if (o == null || getClass() != o.getClass()) return false;
-		FgoServant that = (FgoServant) o;
-		return id == that.id &&
-				Objects.equals(nameZh, that.nameZh) &&
-				Objects.equals(nameEn, that.nameEn) &&
-				Objects.equals(nameJp, that.nameJp) &&
-				Objects.equals(clazz, that.clazz) &&
-				Objects.equals(camp, that.camp) &&
-				Objects.equals(star, that.star) &&
-				Objects.equals(atkStage0, that.atkStage0) &&
-				Objects.equals(hpStage0, that.hpStage0) &&
-				Objects.equals(cards, that.cards) &&
-				Objects.equals(atkStage4, that.atkStage4) &&
-				Objects.equals(hpStage4, that.hpStage4) &&
-				Objects.equals(atkLv90, that.atkLv90) &&
-				Objects.equals(hpLv90, that.hpLv90) &&
-				Objects.equals(atkLv100, that.atkLv100) &&
-				Objects.equals(hpLv100, that.hpLv100) &&
-				Objects.equals(artHit, that.artHit) &&
-				Objects.equals(busterHit, that.busterHit) &&
-				Objects.equals(quickHit, that.quickHit) &&
-				Objects.equals(etxtraHit, that.etxtraHit) &&
-				Objects.equals(painter, that.painter) &&
-				Objects.equals(cv, that.cv) &&
-				Objects.equals(attributes, that.attributes) &&
-				Objects.equals(gender, that.gender) &&
-				Objects.equals(region, that.region) &&
-				Objects.equals(height, that.height) &&
-				Objects.equals(weight, that.weight) &&
-				Objects.equals(critStar, that.critStar) &&
-				Objects.equals(crit, that.crit) &&
-				Objects.equals(npRateArt, that.npRateArt) &&
-				Objects.equals(npRateBuster, that.npRateBuster) &&
-				Objects.equals(npRateQuick, that.npRateQuick) &&
-				Objects.equals(npRateExtra, that.npRateExtra) &&
-				Objects.equals(origin, that.origin) &&
-				Objects.equals(npRateAttacked, that.npRateAttacked) &&
-				Objects.equals(nickname, that.nickname);
-	}
-
-	@Override
-	public int hashCode() {
-
-		return Objects.hash(id, nameZh, nameEn, nameJp, clazz, camp, star, atkStage0, hpStage0, cards, atkStage4, hpStage4, atkLv90, hpLv90, atkLv100, hpLv100, artHit, busterHit, quickHit, etxtraHit, painter, cv, attributes, gender, region, height, weight, critStar, crit, npRateArt, npRateBuster, npRateQuick, npRateExtra, origin, npRateAttacked, nickname);
-	}
 }
